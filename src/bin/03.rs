f9b6a4ac036c0c8a521b1aa64e0f9ef4991b32f4
Jason Zhang <zhangj1079@gmail.com>
2024-12-03 11:58:13 -0500

    day 3, part 2

--- src/bin/03.rs
+++ src/bin/03.rs
@@ -1,3 +1,4 @@
+use itertools::Itertools;
 use regex::Regex;
 
 advent_of_code::solution!(3);
@@ -19,8 +20,32 @@ pub fn part_one(input: &str) -> Option<u32> {
     Some(sum)
 }
 
-pub fn part_two(_input: &str) -> Option<u32> {
-    None
+pub fn part_two(input: &str) -> Option<u32> {
+    let re = Regex::new(r"(mul\([0-9]+,[0-9]+\))|((do|don't)\(\))").unwrap();
+    let nums = Regex::new(r"(?<f>[0-9]+),(?<s>[0-9]+)").unwrap();
+    let instructions: Vec<&str> = re.find_iter(input).map(|str| str.as_str()).collect();
+
+    let mut sum: u32 = 0;
+    let mut toggle: bool = true;
+    for instr in instructions {
+        match instr {
+            "do()" => toggle = true,
+            "don't()" => toggle = false,
+            _ => {
+                if !toggle {
+                    continue;
+                } else {
+                    let captures = nums.captures(instr).unwrap();
+                    let first: u32 = captures.name("f").unwrap().as_str().parse().unwrap();
+                    let second: u32 = captures.name("s").unwrap().as_str().parse().unwrap();
+
+                    sum += first * second;
+                }
+            }
+        }
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -29,13 +54,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        let result = part_one(&advent_of_code::template::read_file_part("examples", DAY, 1));
         assert_eq!(result, Some(161));
     }
 
     #[test]
     fn test_part_two() {
-        let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        let result = part_two(&advent_of_code::template::read_file_part("examples", DAY, 2));
+        assert_eq!(result, Some(48));
     }
 }
